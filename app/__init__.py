da7292c1d07a8ad3045fcd90e3c5f371c1c4d4eb
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-03-05 22:43:35 +0700

    dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,43 @@
 from flask import Flask, render_template, request,  \
-	redirect, url_for
+	redirect, url_for, session
+from app.models.db import Database
 
 app = Flask(__name__)
-
+app.secret_key='f57037d4e48dfd249bc41ef33e15b66a'
+database = Database()
 
 @app.errorhandler(404)
 def page_note_found(e):
 	return render_template('404.html'), 404
 
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/logout')
+def logout():
+	session.clear()
+	return redirect(url_for('index'))
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	error = None
+
 	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+		username = request.form['username']
+		password = request.form['password']
+
+		try:
+			login_data = database.login(username, password)
+
+			if username==login_data[1] and password==login_data[2]:
+				session['name'] = login_data[0]
+				session['username'] = login_data[1]
+				return render_template('dashboard.html', user=login_data[0])
+
+		except TypeError:
 			error = 'Invalid username or password! Please try again.'
-		else:
-			return redirect(url_for('dashboard'))
+			return render_template('index.html', error=error)
 
 	return render_template('index.html', error=error)
 
@@ -23,6 +45,26 @@ def index():
 def dashboard():
 	return render_template('dashboard.html')
 
-@app.route('/about')
-def about():
-	return render_template('about.html')
+@app.route('/data-barang-inventaris')
+def data_inv():
+	return render_template('data-barang-inventaris.html')
+
+@app.route('/input-data-inventaris')
+def input_inv():
+	return render_template('input-data-inventaris.html')
+
+@app.route('/cetak-data-inventaris')
+def cetak_inv():
+	return render_template('cetak-data-inventaris.html')
+
+@app.route('/data-barang-non-inventaris')
+def data_non_inv():
+	return render_template('data-barang-non-inventaris.html')
+
+@app.route('/input-data-non-inventaris')
+def input_non_inv():
+	return render_template('input-data-non-inventaris.html')
+
+@app.route('/cetak-data-non-inventaris')
+def cetak_non_inv():
+	return render_template('cetak-data-non-inventaris.html')
--- app/models/db.py
+++ app/models/db.py
@@ -1,29 +1,41 @@
+from app.models import config
 import pymysql
-import config
 
 
 class Database():
     def __init__(self, db=None, cursor=None):
         self.db = db
         self.cursor = cursor
-    
+        self.host = config.DB_HOST
+        self.user = config.DB_USER
+        self.pwd = config.DB_PASSWORD
+        self.db_name = config.DB_NAME
+
     def openDB(self):
         self.db = pymysql.connect(
-            config.DB_HOST,
-            config.DB_USER,
-            config.DB_PASSWORD,
-            config.DB_NAME
-        )
+            host=self.host,
+            user=self.user,
+            password=self.pwd,
+            db=self.db_name)
         self.cursor = self.db.cursor()
     
     def closeDB(self):
         self.db.close()
 
+    def login(self, username, password):
+        self.openDB()
+        q = "SELECT nm_lengkap, username, password FROM admin \
+            WHERE username='{}' and password='{}'".format(username, password)
+        self.cursor.execute(q)
+        login_data = self.cursor.fetchone()
+        self.closeDB()
+        return login_data
+
     def inputDataInv(self, input_data):
         self.openDB()
         q = "INSERT INTO \
             barang (kd_obat, nm_obat, jenis_obat, asal, tahun, stok, harga, tempat) \
-            values (%s, %s, %s, %s, %s, %s, %s, %s)"%(input_data)
+            values ('{}', '{}', '{}', '{}', {}, {}, {}, '{}')"%(input_data)
         self.cursor.execute(q)
         self.db.commit()
         self.closeDB()
@@ -32,7 +44,7 @@ class Database():
         self.openDB()
         q = "INSERT INTO \
             barangnon (kd_barang, nm_barangnon, jenis_barangnon, asal, tgl_masuk, stok, harga, tempat) \
-            values (%s, %s, %s, %s, %s, %s, %s, %s)"%(input_data)
+            values ({}, {}, {}, {}, {}, {}, {}, {})"%(input_data)
         self.cursor.execute(q)
         self.db.commit()
         self.closeDB()
